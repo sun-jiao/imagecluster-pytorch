6572faefed2db17add25c5a932b4f384f484790d
Sun Jiao <sunjiao@sunjiao.moe>
2020-10-25 10:26:26 +0800

    fix high similarity indexerror

--- examples/example_api_minimal.py
+++ examples/example_api_minimal.py
@@ -3,20 +3,22 @@
 # Minimal example. Use the convenience function io.get_image_data() without any
 # extra arguments.
 
-from imagecluster import calc, io as icio, postproc
+from imagecluster import calc, io, postproc
 
-# The bottleneck is calc.fingerprints() called in this function, all other
-# operations are very fast. get_image_data() writes fingerprints to disk and
-# loads them again instead of re-calculating them.
-images,fingerprints,timestamps = icio.get_image_data('pics/')
+if __name__ == '__main__':
 
-# Run clustering on the fingerprints. Select clusters with similarity index
-# sim=0.5.
-clusters = calc.cluster(fingerprints, sim=0.5)
+    # The bottleneck is calc.fingerprints() called in this function, all other
+    # operations are very fast. get_image_data() writes fingerprints to disk and
+    # loads them again instead of re-calculating them.
+    images,fingerprints,timestamps = io.get_image_data('pics/')
 
-# Create dirs with links to images. Dirs represent the clusters the images
-# belong to.
-postproc.make_links(clusters, 'pics/imagecluster/clusters')
+    # Run clustering on the fingerprints. Select clusters with similarity index
+    # sim=0.5.
+    clusters = calc.cluster(fingerprints, sim=0.9)
 
-# Plot images arranged in clusters.
-postproc.visualize(clusters, images)
+    # Create dirs with links to images. Dirs represent the clusters the images
+    # belong to.
+    postproc.make_links(clusters, 'clusters')
+
+    # Plot images arranged in clusters.
+    postproc.visualize(clusters, images)
